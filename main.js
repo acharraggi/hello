10a4f5c6679d70d57f9042440fe9ee0adc65f2a7
Mike Silversides <mike.silversides@gmail.com>
2017-04-08 09:34:28 -0700

    Update app icon, ScrollView test.

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
 import Expo from 'expo';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, ScrollView, Text, Image, View } from 'react-native';
 
 class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello Mike!</Text>
-      </View>
-    );
-  }
+
+    render() {
+      return (
+          <ScrollView>
+            <Text style={{fontSize:96}}>Scroll me plz</Text>
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Text style={{fontSize:96}}>If you like</Text>
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Text style={{fontSize:96}}>Scrolling down</Text>
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Text style={{fontSize:96}}>What's the best</Text>
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Text style={{fontSize:96}}>Framework around?</Text>
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Image source={require('./assets/icons/app.png')} />
+            <Text style={{fontSize:80}}>React Native</Text>
+          </ScrollView>
+      );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 Expo.registerRootComponent(App);
